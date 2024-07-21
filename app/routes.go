104ab935f1fee8da336b4881dd3df419fa68f262
Dani <dani.webdev@gmail.com>
2024-07-21 07:54:50 +0700

    remove old response for `/`

--- app/routes.go
+++ app/routes.go
@@ -6,10 +6,6 @@ import "github.com/gofiber/fiber/v2"
 
 func RouteInit(app *fiber.App) {
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Ok!")
-	})
-
 	app.Get("/", AppMiddleware, getAllRecords)
     app.Post("/", AppMiddleware, insertRecord)
     app.Put("/:id", AppMiddleware, updateRecord)
