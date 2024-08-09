2eb810286c4fefe0861823e0d900ff76c2fcf0ad
Dani <dani.webdev@gmail.com>
2024-08-09 10:41:45 +0700

    fix align

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
     return (
         <header className="dark:bg-dark-800 border-b border-b-dark-500 body-font">
-            <div className="max-w-screen-xl flex justify-between mx-auto py-3 px-10">
+            <div className="max-w-screen-xl flex justify-between items-center mx-auto py-3 px-10">
                 <div className="flex justify-start items-center">
                     <div className="w-[100px]">
                         <Image src="/logo.svg" alt="logo" width={80} height={80}></Image>
@@ -12,7 +12,7 @@ export default function Header() {
                 </div>
                 <div>
                     <a href="https://github.com/daniwebdev/json.web.id" target="_blank" className="inline-flex items-center text-white">
-                        <Image src="/github-logo-white.svg" alt="github logo" width={20} height={20}></Image>
+                        <Image src="/github-logo-white.svg" alt="github logo" width={30} height={30}></Image>
                     </a>
                 </div>
             </div>
