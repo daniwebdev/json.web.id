682eee1f646acee6d0db11a0356ff30a8b4198c4
Dani <dani.webdev@gmail.com>
2024-08-09 10:21:28 +0700

    remove cors

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/daniwebdev/api-json-web-id/app"
 	"github.com/daniwebdev/api-json-web-id/config"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -22,7 +21,7 @@ func main() {
 	server := fiber.New()
 
 	// Use CORS middleware with default configuration
-	server.Use(cors.New())
+	// server.Use(cors.New())
 	// server.Use(cors.New(cors.Config{
 	// 	AllowOrigins: "*", // Allow all origins
 	// 	AllowMethods: "GET,POST,PUT,DELETE,OPTIONS", // Allow GET, POST, and OPTIONS methods
